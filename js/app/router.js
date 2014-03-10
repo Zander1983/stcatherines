d15d9f1f16a16fc52ad76a3dc58a4b95b4aeed60
web-intelligence <info@webintelligence.ie>
2014-03-10 18:47:54 +0000

    constcants

--- js/app/router.js
+++ js/app/router.js
@@ -215,8 +215,7 @@ define(function (require) {
                         update:false,
                         success: function (collection) {
                             Useful.correctView(that.body);
-                            alert('in generic and is_push is');
-                            alert(is_push);
+        
                             if(is_push===false){
                                 slider.slidePage(new GenericList({collection: collection}).$el);                         
                             }
